c8f0de776d54ed4c3decc7164b97922e32afb86a
Gregor Johnson <gregor.johnson@outlook.com>
2025-12-08 21:52:10 -0600

    updated package json

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
-    domains: ['img.clerk.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+      },
+    ],
   },
-  eslint: {
-    ignoreDuringBuilds: true, 
-  },
-
 };
 
 export default nextConfig;
